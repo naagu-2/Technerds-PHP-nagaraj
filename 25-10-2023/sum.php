58785d607652a7d7ad62aab2127fda3a9428e140
Nagaraj B M <nagarajbm2004@gmail.com>
2023-10-25 16:44:01 +0530

    phps

---
+++ 25-10-2023/sum.php
@@ -0,0 +1,10 @@
+<?php
+$g=" sum is";
+$s="+";
+$a=10;
+$b=20;
+$c=$a+$b;
+echo " $a $s $b  $g $c";
+  echo "<br>";
+var_dump ($a);
+?>
---
+++ 25-10-2023/variable.php
@@ -0,0 +1,18 @@
+<?php
+$name="hello world";
+$age=13;
+$salary=400.00;
+$type=TRUE;
+echo $name;
+echo "<br>";
+echo $age;
+echo "<br>";
+echo $type;
+
+
+
+
+
+
+
+?>
