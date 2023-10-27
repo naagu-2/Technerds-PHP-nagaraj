9f846b7b630414d658e88fd7113bbad5b0bb0a2c
Nagaraj B M <nagarajbm2004@gmail.com>
2023-10-27 17:13:07 +0530

    ts

---
+++ 27-10-23/form.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <style>
+    div{
+        color:blue;
+        margin: left 80px;  
+        
+        border:2px solid green;
+    }
+    #ss{
+       color:solid black 3px;
+       border:2px solid green;
+       margin:left 50px;
+    }
+
+   </style>
+    
+</head>
+<body>
+    <div>
+    <form Action="https://www.shiksha.com/college/ndrk-institute-of-technology-hassan-49353" >
+        <label for=""> <b>DATE</b></label>
+        <input  type="date" ><br><br>
+        <label for=""> <b>MONTH</b></label>
+        <input type="week" name="" id=""><br><br>
+        <label for="name"> <b>User name </b>:</label>
+        <input type="text" name="name" placeholder="Username" ><br><br>
+        <label for="Phone"> <b>Phone-no</b> :</label>
+        <input type="tel" name="phone" placeholder="Contact no" ><br><br>
+       
+        <label for="mail"> <b>E-Mail </b>:</label>
+        <input type="email" name="mail"  placeholder="Email" ><br><br>
+        <label for="pass"> <b>Password</b></label>
+        <input type="password" name="pass" placeholder="Password"><br><br>
+        <label for="UPLOAD PHOTO"> <b>UPLOAD PHOTO (2*2) </b>:</label>
+        <input type="file"><br><br>
+        <label for="Cource"> <b>Select Cource</b></label>
+        <select name="Cource" >
+            <option value="cource">SELECT</option>
+            <option value="cource">BCA</option>
+            <option value="cource">BBA</option>
+            <option value="cource">B-ED</option>
+        </select><br><br>
+        <input type="submit" value="submit" id="ss"><br>
+        
+        
+        
+
+    </form>
+    </div>
+    
+</body>
+</html>
